9aaebaf28a4490860056c2fda8fdec06cabbd661
Wesley Menezes <www.wesley+usa@gmail.com>
2019-12-27 23:22:58 -0300

    Versão 1.0

--- devicetypes/br-com-wesley/zigbee-smart-switch-child.src/zigbee-smart-switch-child.groovy
+++ devicetypes/br-com-wesley/zigbee-smart-switch-child.src/zigbee-smart-switch-child.groovy
@@ -23,9 +23,6 @@ metadata {
         capability "Switch"
 	}
     
-    command "off"
-    command "on"
-
 	tiles(scale: 2) {
 		standardTile ("switch", "device.switch", width: 2, height: 2, canChangeIcon: true, decoration: "flat") {
 			state ("off", label: '${name}', action: "on", icon: "st.switches.light.off", backgroundColor: "#ffffff", nextState: "turningOn")
@@ -40,30 +37,14 @@ metadata {
 }
 
 def createAndSendEvent(map) {
-    //if (map?.name == "switch") { map.name = "switchChild" }
 	log.debug "sendEvent($map)"
 	sendEvent(map)
-    return map
+    map
 }
 
-def on() {
-    parent.on(device)
-}
+def on() { parent.on(device) }
 
-def off() {
-    parent.off(device)
-}
-
-def parse (description) {
-	log.debug "[PARSE-CHILD] description: $description, cluster: ${zigbee.parse(description)}, event: ${zigbee.getEvent(description)}, map: ${zigbee.parseDescriptionAsMap(description)}"
-	if (description?.startsWith("on/off: ")) {
-        //return createEvent(name: "switch", value: device.currentValue("switch"))
-//        return createEvent(name: "switch", value: state.turning[0]?:calculateValue(device.currentValue("childSwitch0")))
-    }
-    def map = zigbee.parseDescriptionAsMap(description)
-    log.debug "DID NOT PARSE MESSAGE for description : $description"
-    log.debug map
-}
+def off() { parent.off(device) }
 
 def poll() {
 	log.debug "poll()"
@@ -83,7 +64,7 @@ def refresh() {
 def installed () {
 	log.debug "installed() - parent $parent"
 
-    sendEvent(name: "checkInterval", value: 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+    sendEvent(name: "checkInterval", value: 12 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
 }
 
 def uninstalled () {
